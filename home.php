de806c9ee73365d1d40d4edd1a23f43c260f82c6
masgcode <masgcode@hotmail.com>
2021-03-18 22:32:10 -0600

    1.1

--- home.php
+++ home.php
@@ -6,11 +6,116 @@
     <meta name="author" content="Michael Salas">
     <title>Home</title>
 
-    <!-- Link de los archivos de estilos css -->
-    <link rel="stylesheet" href="vista/css/login.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="vista/css/home.css">
+    <link rel="stylesheet" href="vista/css/tabla.css">
+    <link rel="stylesheet" href="vista/css/dashboard.css">
+
 </head>
 <body>
-    <?php echo $_SESSION['usuario']; ?>
-    <a href="datos/logout.php">Cerrar sesión</a>
+    
+    <div class="contenedor">
+
+        <!-- MENU IZQUIERDA -->
+        <div class="contenedor-menu">
+            <div class="menu">
+            <a class="etiqueta-usuario" href="#">Inicio</a>
+                <nav>
+                    <a href="#">Entradas</a>
+                    <a href="#">Cuarto Limpio</a>
+                    <a href="#">Cuarto Sucio</a>
+                    <a href="#">Proceso Termico</a>
+                    <a href="#">Paletizado</a>
+                    <a href="#">Bodega</a>
+                    <a href="#">Productividad</a>
+                </nav>
+            </div>
+        </div>
+        <!-- MENU IZQUIERDA -->
+
+        <div class="subcontenedor">
+
+            <!-- MENU SUPERIOR -->
+            <div id="navbar">
+                <a href="#"><?php echo $_SESSION['usuario']; ?></a>
+                <a href="datos/logout.php">Cerrar sesión</a>
+            </div>
+            <!-- MENU SUPERIOR -->
+    
+            <!-- TABLA -->
+            <div id="contenedor-1">
+                <div id="encabezado-1"></div>
+                <div id="contenedor-2">
+                    <div id="encabezado-2">
+                        <label>Colaboradores</label>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>estudiante</th>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Edad</th>
+                                <th>Teléfono</th>
+                                <th>Accion</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label="id Estudiante">1</td>
+                                <td data-label="nombre Estudiante">juan</td>
+                                <td data-label="Apellido Estudiante">buriticá</td>
+                                <td data-label="edad Estudiante">21</td>
+                                <td data-label="telefono">455754</td>
+                                <td data-label="telefono">Editar Eliminar</td>
+                            </tr>
+                            <tr>
+                                <td data-label="id Estudiante">2</td>
+                                <td data-label="nombre Estudiante">jose</td>
+                                <td data-label="Apellido Estudiante">castaño</td>
+                                <td data-label="edad Estudiante">18</td>
+                                <td data-label="telefono">455754</td>
+                                <td data-label="telefono">Editar Eliminar</td>
+                            </tr>	    	
+                            <tr>
+                                <td data-label="id Estudiante">3</td>
+                                <td data-label="nombre Estudiante">fernado</td>
+                                <td data-label="Apellido Estudiante">lopez</td>
+                                <td data-label="edad Estudiante">16</td>
+                                <td data-label="telefono">455754</td>
+                                <td data-label="telefono">Editar Eliminar</td>
+                            </tr>
+                            <tr>
+                                <td data-label="id Estudiante">1</td>
+                                <td data-label="nombre Estudiante">juan</td>
+                                <td data-label="Apellido Estudiante">buriticá</td>
+                                <td data-label="edad Estudiante">21</td>
+                                <td data-label="telefono">455754</td>
+                                <td data-label="telefono">Editar Eliminar</td>
+                            </tr>
+                            <tr>
+                                <td data-label="id Estudiante">2</td>
+                                <td data-label="nombre Estudiante">jose</td>
+                                <td data-label="Apellido Estudiante">castaño</td>
+                                <td data-label="edad Estudiante">18</td>
+                                <td data-label="telefono">455754</td>
+                                <td data-label="telefono">Editar Eliminar</td>
+                            </tr>	    	
+                            <tr>
+                                <td data-label="id Estudiante">3</td>
+                                <td data-label="nombre Estudiante">fernado</td>
+                                <td data-label="Apellido Estudiante">lopez</td>
+                                <td data-label="edad Estudiante">16</td>
+                                <td data-label="telefono">455754</td>
+                                <td data-label="telefono">Editar Eliminar</td>
+                            </tr>
+                    </table>
+                </div>
+            </div>
+            <!-- TABLA -->
+
+        </div>
+    </div>
+    
 </body>
 </html>
